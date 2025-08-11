dd57f8a4b0c9023c18a48fb96e9800b969e44a52
Chapati0650 <prithviplayz1019@gmail.com>
2025-08-11 10:13:28 -0500

    Fix MathRenderer import path

--- src/App.tsx
+++ src/App.tsx
@@ -415,17 +415,17 @@ const QuestionGenerator = () => {
 
   const handleStartReview = () => {
     console.log('🔍 Starting review mode...');
-    setIsReviewMode(true);
   };
 
   const handleExitReview = () => {
     console.log('🚪 Exiting review mode...');
+    setIsReviewMode(false);
   };
-
   const savePracticeSessionToDb = async () => {
     try {
       const correctAnswers = questions.filter((question, index) => {
         if (question.questionType === 'multiple_choice') {
+          return answers[index] === question.correctAnswer;
         } else {
           return openEndedAnswers[index] === question.correctAnswerText;
         }
